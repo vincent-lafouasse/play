5c0213979435b04573a892d84a2133b1957bcb2e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-10-20 21:51:26 +0200

    define macros for format codes

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@
 #include "read.h"
 
 [[maybe_unused]] const char* wav = "./wav/nice_chord.wav";
+#define WAVE_FORMAT_PCM 0x0001
+#define WAVE_FORMAT_IEEE_FLOAT 0x0003
+#define WAVE_FORMAT_ALAW 0x0006
+#define WAVE_FORMAT_MULAW 0x0007
+#define WAVE_FORMAT_EXTENSIBLE 0xFFFE
 
 int main() {
     std::ifstream input(wav, std::ios::binary | std::ios::in);
