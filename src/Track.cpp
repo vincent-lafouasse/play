256e72e5aa2c82dc758c32f9c76f353772ad570f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-10-20 22:04:47 +0200

    refactor

--- src/Track.cpp
+++ src/Track.cpp
@@ -1,1 +1,27 @@
 #include "Track.h"
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+
+#include "read.h"
+
+Track::Track(const std::string& path) {
+    std::ifstream input(path, std::ios::binary | std::ios::in);
+    if (!input.is_open()) {
+        std::cerr << "Failed to open file " << path << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+
+    std::vector<uint8_t> bytes(std::istreambuf_iterator<char>(input), {});
+    size_t index = 0;
+
+    std::cout << LittleEndian::read_fourcc(bytes, index) << std::endl;
+    std::cout << LittleEndian::read_u32(bytes, index) << std::endl;
+    std::cout << LittleEndian::read_fourcc(bytes, index) << std::endl;
+    std::cout << LittleEndian::read_fourcc(bytes, index) << std::endl;
+    std::cout << LittleEndian::read_u32(bytes, index) << std::endl;
+    std::cout << LittleEndian::read_u16(bytes, index) << std::endl;
+    std::cout << LittleEndian::read_u16(bytes, index) << std::endl;
+    std::cout << LittleEndian::read_u32(bytes, index) << std::endl;
+    std::cout << LittleEndian::read_u32(bytes, index) << std::endl;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
-#include <cstdint>
-#include <fstream>
 #include <iostream>
 
-#include "read.h"
+#include "Track.h"
 
 #define WAVE_FORMAT_PCM 0x0001
 #define WAVE_FORMAT_IEEE_FLOAT 0x0003
@@ -18,23 +16,5 @@ int main(int ac, char* av[]) {
         std::exit(EXIT_FAILURE);
     }
 
-    const char* path = av[1];
-    std::ifstream input(path, std::ios::binary | std::ios::in);
-    if (!input.is_open()) {
-        std::cerr << "Failed to open file " << path << std::endl;
-        std::exit(EXIT_FAILURE);
-    }
-
-    std::vector<uint8_t> bytes(std::istreambuf_iterator<char>(input), {});
-    size_t index = 0;
-
-    std::cout << LittleEndian::read_fourcc(bytes, index) << std::endl;
-    std::cout << LittleEndian::read_u32(bytes, index) << std::endl;
-    std::cout << LittleEndian::read_fourcc(bytes, index) << std::endl;
-    std::cout << LittleEndian::read_fourcc(bytes, index) << std::endl;
-    std::cout << LittleEndian::read_u32(bytes, index) << std::endl;
-    std::cout << LittleEndian::read_u16(bytes, index) << std::endl;
-    std::cout << LittleEndian::read_u16(bytes, index) << std::endl;
-    std::cout << LittleEndian::read_u32(bytes, index) << std::endl;
-    std::cout << LittleEndian::read_u32(bytes, index) << std::endl;
+    Track track(av[1]);
 }
