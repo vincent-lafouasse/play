d647aed8ab51b656b2d3a3dcfae2506f05b8ab6d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-10-20 22:18:29 +0200

    keep deserializing wav

--- src/Track.cpp
+++ src/Track.cpp
@@ -6,6 +6,17 @@
 
 #include "read.h"
 
+#define WAVE_FORMAT_PCM 0x0001
+#define WAVE_FORMAT_IEEE_FLOAT 0x0003
+#define WAVE_FORMAT_ALAW 0x0006
+#define WAVE_FORMAT_MULAW 0x0007
+#define WAVE_FORMAT_EXTENSIBLE 0xFFFE
+
+template <typename T>
+void log_var(const T& e) {
+    std::cout << e << std::endl;
+}
+
 Track::Track(const std::string& path) {
     std::ifstream input(path, std::ios::binary | std::ios::in);
     if (!input.is_open()) {
@@ -19,4 +30,15 @@ Track::Track(const std::string& path) {
     assert(LittleEndian::read_fourcc(bytes, index) == "RIFF");
     LittleEndian::read_u32(bytes, index);  // discard chunk size
     assert(LittleEndian::read_fourcc(bytes, index) == "WAVE");
+
+    assert(LittleEndian::read_fourcc(bytes, index) == "fmt ");
+    uint32_t fmt_chunk_sz = LittleEndian::read_u32(bytes, index);
+    log_var<uint32_t>(fmt_chunk_sz);
+    assert(LittleEndian::read_u16(bytes, index) == WAVE_FORMAT_PCM);
+    uint16_t n_channels = LittleEndian::read_u16(bytes, index);
+    log_var<uint16_t>(n_channels);
+    uint32_t blocks_per_sec = LittleEndian::read_u32(bytes, index);
+    uint32_t data_rate = LittleEndian::read_u32(bytes, index);
+    uint16_t block_size = LittleEndian::read_u32(bytes, index);
+    uint16_t sample_size = LittleEndian::read_u32(bytes, index);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,6 @@
 
 #include "Track.h"
 
-#define WAVE_FORMAT_PCM 0x0001
-#define WAVE_FORMAT_IEEE_FLOAT 0x0003
-#define WAVE_FORMAT_ALAW 0x0006
-#define WAVE_FORMAT_MULAW 0x0007
-#define WAVE_FORMAT_EXTENSIBLE 0xFFFE
-
 #define USAGE "play WAV_FILE"
 
 int main(int ac, char* av[]) {
