3721c640ca79414eb269b9db381ef38345c74268
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-10-20 20:59:17 +0200

    format

--- src/read.cpp
+++ src/read.cpp
@@ -6,4 +6,4 @@ namespace LittleEndian {
 [[maybe_unused]] uint32_t read_u32(std::istream is);
 [[maybe_unused]] uint64_t read_u64(std::istream is);
 [[maybe_unused]] std::string read_fourcc(std::istream is);
-}
+}  // namespace LittleEndian
--- src/read.h
+++ src/read.h
@@ -10,4 +10,4 @@ namespace LittleEndian {
 [[maybe_unused]] uint32_t read_u32(std::istream is);
 [[maybe_unused]] uint64_t read_u64(std::istream is);
 [[maybe_unused]] std::string read_fourcc(std::istream is);
-}
+}  // namespace LittleEndian
