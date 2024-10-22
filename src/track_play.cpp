679d4d4c4ac9c9f13650e9f8ee54ce78a0cc518e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-10-22 16:32:45 +0200

    sketch mono playback

--- src/track_play.cpp
+++ src/track_play.cpp
@@ -1,18 +1,62 @@
-#include "OutputStream.h"
+#include <portaudio.h>
+#include <iostream>
 #include "Track.h"
 
-#define BUFFER_IZE 256
+#define BUFFER_SIZE 256
 
 struct MonoPlaybackContext {
-    std::vector<float> samples;
+    const std::vector<float>* samples;
     size_t i;
 };
 
-int callback(const void* input_buffer,
+struct StreamConfig {
+    int n_in;
+    int n_out;
+    double sample_rate;
+    unsigned long buffer_size;
+};
+
+int callback(const void* _input_buffer,
              void* output_buffer,
              unsigned long buffer_size,
-             const PaStreamCallbackTimeInfo* time_info,
-             PaStreamCallbackFlags status_flags,
-             void* user_data);
+             const PaStreamCallbackTimeInfo* _time_info,
+             PaStreamCallbackFlags _status_flags,
+             void* user_data) {
+    (void)_input_buffer;
+    (void)_time_info;
+    (void)_status_flags;
 
-void Track::play() const {}
+    float* buffer = static_cast<float*>(output_buffer);
+    MonoPlaybackContext* ctx = static_cast<MonoPlaybackContext*>(user_data);
+
+    for (size_t i = 0; i < buffer_size; i++) {
+        if (ctx->i < ctx->samples->size()) {
+            buffer[i] = ctx->samples->at(ctx->i);
+            ctx->i++;
+        } else {
+            buffer[i] = 0.0;
+        }
+    }
+
+    return 0;
+}
+
+static void check_error(PaError err);
+
+void Track::play() const {
+    StreamConfig cfg = {0, 1, static_cast<double>(this->sampling_rate()),
+                        BUFFER_SIZE};
+    MonoPlaybackContext ctx = {&m_data[0], 0};
+
+    PaStream* stream;
+    check_error(Pa_OpenDefaultStream(&stream, cfg.n_in, cfg.n_out, paFloat32,
+                                     cfg.sample_rate, cfg.buffer_size, callback,
+                                     &ctx));
+}
+
+static void check_error(PaError err) {
+    if (err != paNoError) {
+        std::cerr << Pa_GetErrorText(err) << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+}
