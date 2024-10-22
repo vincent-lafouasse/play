5f5d2bab0f11081a8d0b0bbb7e51bb2f3a3a9d2b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-10-22 21:57:06 +0200

    refactor

--- src/track_play.cpp
+++ src/track_play.cpp
@@ -19,30 +19,12 @@ struct StreamConfig {
 
 static void check_error(PaError err);
 
-int find_output_device(int n_out, double sample_rate) {
-    int n_devices = Pa_GetDeviceCount();
-    if (n_devices < 0)
-        check_error(n_devices);
-
-    for (int device = 0; device < n_devices; device++) {
-        const PaDeviceInfo* deviceInfo = Pa_GetDeviceInfo(device);
-        if (deviceInfo->maxOutputChannels < n_out)
-            continue;
-
-        PaStreamParameters candidate = {device, n_out, paFloat32, deviceInfo->defaultLowOutputLatency, NULL};
-        if (Pa_IsFormatSupported(NULL, &candidate, sample_rate) == paFormatIsSupported)
-            return device;
-    }
-
-    return -1;
-}
-
 int mono_playback(const void* _input_buffer,
-             void* output_buffer,
-             unsigned long buffer_size,
-             const PaStreamCallbackTimeInfo* _time_info,
-             PaStreamCallbackFlags _status_flags,
-             void* user_data) {
+                  void* output_buffer,
+                  unsigned long buffer_size,
+                  const PaStreamCallbackTimeInfo* _time_info,
+                  PaStreamCallbackFlags _status_flags,
+                  void* user_data) {
     (void)_input_buffer;
     (void)_time_info;
     (void)_status_flags;
@@ -70,10 +52,11 @@ void Track::play() const {
     PaStream* stream;
 
     check_error(Pa_Initialize());
-    check_error(Pa_OpenDefaultStream(&stream, cfg.n_in, cfg.n_out, paFloat32, cfg.sample_rate, cfg.buffer_size, mono_playback, &ctx));
+    check_error(Pa_OpenDefaultStream(&stream, cfg.n_in, cfg.n_out, paFloat32,
+                                     cfg.sample_rate, cfg.buffer_size,
+                                     mono_playback, &ctx));
     check_error(Pa_StartStream(stream));
-    while (1)
-    {
+    while (1) {
         Pa_Sleep(2000);
     }
     check_error(Pa_Terminate());
